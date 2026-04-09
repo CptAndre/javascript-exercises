e1a1acdd7ebf7ce9736ab383536607f3191a90fd
CptAndre <138729600+CptAndre@users.noreply.github.com>
2026-04-09 07:24:59 -0300

    /foundations/07 solution

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,20 @@
-const reverseString = function() {
+/* 
 
+Started with logic method
+
+const reverseString = function(string) {
+let reverse = "";
+
+ for(let i=string.length-1; i>=0 ; i--){
+    reverse+=string[i];
+ }
+ return reverse;
+}; */
+
+// Ended with js functions after research
+
+const reverseString = function(string) {
+ return string.split("").reverse().join("");
 };
 
 // Do not edit below this line
