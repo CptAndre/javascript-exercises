be425972ca8dc0338d0d59e2073ce590b0862bac
CptAndre <138729600+CptAndre@users.noreply.github.com>
2026-04-10 16:00:20 -0300

    /foundations/10 solution

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,28 @@
-const leapYears = function() {
+/* 
+did directly this first
 
+const leapYears = function(year) {
+    if(year % 4 === 0){
+        if(!(year % 100 === 0) || year % 400 === 0){
+        return true;
+    }
+}
+return false;
+}; */
+
+// but solution provided me with a clearer code to everyone (including me) to read
+
+const leapYears = function(year) {
+    let checkDivisibleFour = year % 4 === 0;
+    let checkCeuntry = year % 100 === 0;
+    let checkDivisibleFourHundred = year % 400 === 0;
+    
+    if(checkDivisibleFour){
+        if(!(checkCeuntry) || checkDivisibleFourHundred){
+        return true;
+        }
+    }
+    return false;
 };
 
 // Do not edit below this line
