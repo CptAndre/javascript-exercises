b66ddb015c33ab177c3d2e8f536440ade942801d
CptAndre <138729600+CptAndre@users.noreply.github.com>
2026-04-10 17:53:09 -0300

    /foundations/11 solution

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,28 @@
-const convertToCelsius = function() {
+/* 
+
+direct usage in return using toFixed
+
+const convertToCelsius = function(temp) {
+  return Number(((temp - 32) * 5 / 9).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  return Number((temp * 9 / 5 + 32).toFixed(1));
 };
+
+*/
+
+// version with Math.round and variable 
+
+const convertToCelsius = function(temp) {
+  let conversion = (temp - 32) * 5 / 9;
+  return Math.round(conversion * 10) / 10;
+};
+
+const convertToFahrenheit = function(temp) {
+  let conversion = temp * 9 / 5 + 32;
+  return Math.round(conversion * 10) / 10;
+}
 
 // Do not edit below this line
 module.exports = {
