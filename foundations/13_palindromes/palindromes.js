a7ec01000b2035a32218848442a297833a337776
CptAndre <138729600+CptAndre@users.noreply.github.com>
2026-04-23 17:05:57 -0300

    /foundations/13 solution

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,4 +1,18 @@
-const palindromes = function () {
+const reverseString = require("../07_reverseString/reverseString");
+
+const palindromes = function (string) {
+
+const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789'; 
+
+let normalString = string.toLowerCase().
+                                split("").
+                                filter((character) => alphanumerical.includes(character)).
+                                reverse().
+                                join("");           
+
+const reversedString = normalString.split('').reverse().join('');
+
+return normalString === reversedString;
 
 };
 
