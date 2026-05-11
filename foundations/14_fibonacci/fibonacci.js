1e75b276c28dbdd87bd8b90f3bdf700f114ee79c
CptAndre <138729600+CptAndre@users.noreply.github.com>
2026-05-11 15:01:35 -0300

    /foundations/14 solution

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(value) {
+  
+  valuechange = parseInt(value);
 
+  if(valuechange < 0){
+    return "OOPS";
+  }
+
+  if(valuechange == 0){
+    return 0;
+  }
+
+  if(valuechange == 2 || valuechange == 1){
+    return 1;
+  }
+  
+  let a = 1;
+  let b = 1;
+  for (let i = 3; i <= valuechange; i++) {
+    let c = a + b;
+    a = b;
+    b = c;
+  }
+  return b;
 };
 
 // Do not edit below this line
