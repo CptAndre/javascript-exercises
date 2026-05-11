b645bffb082932630a06b03281ba1b227c351f8d
CptAndre <138729600+CptAndre@users.noreply.github.com>
2026-05-11 17:08:28 -0300

    /foundations/16 solution

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  return people.reduce((oldest, person) => {
+    const personAge = (person.yearOfDeath || 2026) - person.yearOfBirth;
+    const oldestAge = (oldest.yearOfDeath || 2026) - oldest.yearOfBirth;
+    return personAge > oldestAge ? person : oldest;
+  });
 };
 
 // Do not edit below this line
